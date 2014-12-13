6d109664d8d9aaefc5bfb827278752fe67e56bc3
kadensotek <rrj09832@gmail.com>
2014-12-12 23:25:27 -0500

    Added comments and increased modularity

--- src/main/com/rrj09832/rjob/RobertJonesOrderBook.java
+++ src/main/com/rrj09832/rjob/RobertJonesOrderBook.java
@@ -8,41 +8,32 @@ public class RobertJonesOrderBook
     {
         Market myMarket = new Market();
         myMarket.openMarket();
-        myMarket.addBid(21.35, 25);
-        myMarket.addOffer(20.0, 30);
-        myMarket.addBid(12.35, 12);
-        myMarket.addBid(12.35, 15);
-        myMarket.addBid(12.36, 45);
-        myMarket.addBid(13.0, 11);
-        myMarket.addBid(12.35, 12);
-        myMarket.addBid(12.34, 40);
-        myMarket.printBids();
+//
+//        myMarket.addBid(21.35, 25);
+//        myMarket.addOffer(20.0, 30);
+//        myMarket.addBid(12.35, 12);
+//        myMarket.addBid(12.35, 15);
+//        myMarket.addBid(12.36, 45);
+//        myMarket.addBid(13.0, 11);
+//        myMarket.addBid(12.35, 12);
+//        myMarket.addBid(12.34, 40);
+//        myMarket.addOffer(20.0, 30);
+//        myMarket.addOffer(20.0, 60);
+//        myMarket.addOffer(11.0, 30);
+//        myMarket.addOffer(25.0, 18);
+//        myMarket.addOffer(12.0, 20);
+//        myMarket.addOffer(12.0, 15);
+//        myMarket.addOffer(15.0, 17);
 
-        myMarket.addOffer(20.0, 30);
-        myMarket.addOffer(20.0, 60);
-        myMarket.addOffer(11.0, 30);
-        myMarket.addOffer(25.0, 18);
-        myMarket.addOffer(2.0, 20);
-        myMarket.addOffer(2.0, 15);
-        myMarket.addOffer(15.0, 17);
-        myMarket.printOffers();
+        myMarket.addOffer(20.30, 100);
+        myMarket.addOffer(20.25, 100);
+        myMarket.addOffer(20.30, 200);
+        myMarket.addBid(20.15, 100);
+        myMarket.addBid(20.20, 200);
+        myMarket.addBid(20.15, 200);
+        myMarket.addBid(20.35, 250);
+
+        myMarket.closeMarket();
 
     }
-
 }
-
-
-/*
-    Open market
-
-    Add selling info
-
-    Add buying orders
-
-    Match and close trades
-        Pricetime priority
-        Report successful trades as they happen
-
-    Close market
-        Report cancelled trades
- */
